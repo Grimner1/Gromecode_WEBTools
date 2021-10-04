40ed42e3b64bf7dbd19c957a124ad260bffb91e6
Grimner1 <ursuld@gmail.com>
2021-10-04 22:12:25 +0300

    2-0

---
+++ lesson8/test2/webpack.config.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+  const config = {
+    entry: "./src/list/index.js",
+    output: {
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/i,
+          use: ["babel-loader"],
+        },
+        {
+          test: /\.s?css$/i,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style.loader",
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.(png|jpg|gif)$/i,
+          use: [
+            {
+              loader: "url-loader",
+              options: {
+                outputPath: "images",
+                limit: 8192,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
+    ],
+    devServer: {
+      hot: true,
+      port: 9000,
+    },
+  };
+
+  if (isProduction) {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+      })
+    );
+  }
+
+  return config;
+};
